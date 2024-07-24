701bb35a4359e376df3222dc4d71062e31c00ca7
Woojin Kwak <65993764+gugyeoj1n@users.noreply.github.com>
2024-07-24 09:54:37 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char a;
+    cin >> a;
+    cout << int(a);
+
+    return 0;
+}
