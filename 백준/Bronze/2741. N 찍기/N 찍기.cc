c5bd756fee01a46d955903d0d256ef86e30c261d
Woojin Kwak <65993764+gugyeoj1n@users.noreply.github.com>
2024-07-24 09:40:53 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+        cout << i << "\n";
+
+    return 0;
+}
