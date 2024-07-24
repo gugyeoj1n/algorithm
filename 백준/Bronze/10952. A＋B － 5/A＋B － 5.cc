16cfe7c45d8406cc9dc96082a75012862b696e3d
Woojin Kwak <65993764+gugyeoj1n@users.noreply.github.com>
2024-07-24 09:52:03 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b;
+
+    while(true) {
+        cin >> a >> b;
+        if(a == 0 && b == 0)
+            break;
+        cout << a + b << "\n";
+    }
+
+    return 0;
+}
